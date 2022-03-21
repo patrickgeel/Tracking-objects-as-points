e000f10ccf36c778b77549ebef43e7333c61095d
Patrick G <44997541+patrickgeel@users.noreply.github.com>
2022-03-21 17:37:31 +0100

    Update run_sweep.sh

--- src/run_sweep.sh
+++ src/run_sweep.sh
@@ -2,5 +2,6 @@
 
 #Train the model
 python main.py tracking --exp_id "$1" --dataset mot --dataset_version 17halftrain --num_epochs 1 --batch_size 10 --lr_step 60
+cp ../exp/tracking/baselinerun/model_last.pth ../models/"$1".pth
 # Run test on the trained model 
 python test.py tracking --exp_id "$1" --dataset mot --dataset_version 17halfval --pre_hm --ltrb_amodal --track_thresh 0.4 --pre_thresh 0.5 --load_model ../exp//tracking/"$1"/model_last.pth
