73ecb108e83c372b97a8b3a8780aacc9007222cd
patrickgeel <patrickgeel10@deeplearning-1-vm.europe-west4-a.c.tracking-objects-as-points.internal>
2022-03-22 09:25:26 +0000

    logging

--- src/run_sweep.sh
+++ src/run_sweep.sh
@@ -3,4 +3,4 @@
 #Train the model
 python main.py tracking --exp_id "$1" --dataset mot --dataset_version 17halftrain --num_epochs "$2" --batch_size "$3" --lr_step "$4"
 # Run test on the trained model 
-python test.py tracking --exp_id "$1" --dataset mot --dataset_version 17halfval --pre_hm --ltrb_amodal --track_thresh 0.4 --pre_thresh 0.5 --load_model ../exp//tracking/"$1"/model_last.pth
+#python test.py tracking --exp_id "$1" --dataset mot --dataset_version 17halfval --pre_hm --ltrb_amodal --track_thresh 0.4 --pre_thresh 0.5 --load_model ../exp//tracking/"$1"/model_last.pth
