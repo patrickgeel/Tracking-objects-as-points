96dd8186824875cd2c9e0486424f114b67c52b27
patrickg <44997541+patrickgeel@users.noreply.github.com>
2022-03-22 13:23:18 +0100

    Update run_sweep.sh

--- src/run_sweep.sh
+++ src/run_sweep.sh
@@ -1,15 +1,34 @@
 #!/bin/bash 
 
+echo "Enter run name: "  
+read exp_id  
+echo "Enter number of epochs: "  
+read num_epochs
+echo "Enter batch size: "  
+read batch_size
+echo "Enter lr step: "  
+read lr_step
+
+echo "Exp Id: $exp_id"
+echo "Epochs: $num_epochs"
+echo "Batch size: $batch_size"
+echo "Lr Step: $lr_step"
+
 #Train the model
-#python main.py tracking --exp_id "$1" --dataset mot --dataset_version 17halftrain --num_epochs "$2" --batch_size "$3" --lr_step "$4"
+python main.py tracking --exp_id "$exp_id" --dataset mot --dataset_version 17halftrain --num_epochs "$num_epochs" --batch_size "$batch_size" --lr_step "$lr_step"
+
 # Setup logging
+echo "Setup logging directory"
+
 mkdir -p ../data_logging/
-script ../data_logging/"$1.txt"
+script ../data_logging/"$exp_id.txt"
+echo "Logging directory is ../data_logging/$exp_id.txt"
+
 # Run test on the trained model
-python test.py tracking --exp_id "$1" --dataset mot --dataset_version 17halfval --track_thresh 0.4 --pre_thresh 0.5 --load_model ../exp//tracking/"$1"/model_last.pth
+python test.py tracking --exp_id "$exp_id" --dataset mot --dataset_version 17halfval --track_thresh 0.4 --pre_thresh 0.5 --load_model ../exp//tracking/"$exp_id"/model_last.pth
 # Stop logging
 exit
 
-git add ../data_logging/"$1.txt"
-git commit -m "Run $1 is complete"
-git push
+# git add ../data_logging/"$exp_id.txt"
+# git commit -m "Run $exp_id is complete"
+# git push
