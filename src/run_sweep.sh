b3521ef6c79f22fb3b59c4e8e384bd84e1d13ab0
patrickg <44997541+patrickgeel@users.noreply.github.com>
2022-03-22 14:03:59 +0100

    Update run_sweep.sh

--- src/run_sweep.sh
+++ src/run_sweep.sh
@@ -1,22 +1,36 @@
 #!/bin/bash 
 
-# echo "Enter run name: "  
-# read exp_id  
-# echo "Enter number of epochs: "  
-# read num_epochs
-# echo "Enter batch size: "  
-# read batch_size
-# echo "Enter lr step: "  
-# read lr_step
+yes="y"
 
-# echo "Exp Id: $exp_id"
-# echo "Epochs: $num_epochs"
-# echo "Batch size: $batch_size"
-# echo "Lr Step: $lr_step"
+echo "Enter run name: "  
+read exp_id  
+
+echo "Would you like to train a new model? (y/n)"
+read train 
+
+if [ "$train" = "$yes" ]
+then 
+echo "Enter number of epochs: "  
+read num_epochs
+echo "Enter batch size: "  
+read batch_size
+echo "Enter lr step: "  
+read lr_step
+
+echo "Exp Id: $exp_id"
+echo "Epochs: $num_epochs"
+echo "Batch size: $batch_size"
+echo "Lr Step: $lr_step"
+
+else 
+	echo "Exp Id: $exp_id"
+fi
 
 #Train the model
+if [ "$train" = "$yes" ]
+then 
 python main.py tracking --exp_id "$exp_id" --dataset mot --dataset_version 17halftrain --num_epochs "$num_epochs" --batch_size "$batch_size" --lr_step "$lr_step"
-
+fi
 # Setup logging
 echo "Setup logging directory"
 
