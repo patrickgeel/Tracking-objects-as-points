02e3ea04409388f9fa55a78180634b9ab847cae4
patrickg <44997541+patrickgeel@users.noreply.github.com>
2022-03-22 10:35:00 +0100

    Update runsweep

--- src/run_sweep.sh
+++ src/run_sweep.sh
@@ -1,6 +1,15 @@
 #!/bin/bash 
 
 #Train the model
-python main.py tracking --exp_id "$1" --dataset mot --dataset_version 17halftrain --num_epochs "$2" --batch_size "$3" --lr_step "$4"
+#python main.py tracking --exp_id "$1" --dataset mot --dataset_version 17halftrain --num_epochs "$2" --batch_size "$3" --lr_step "$4"
+# Setup logging
+mkdir -p ../data_logging/
+script ../data_logging/"$1.txt"
 # Run test on the trained model 
-python test.py tracking --exp_id "$1" --dataset mot --dataset_version 17halfval --pre_hm --ltrb_amodal --track_thresh 0.4 --pre_thresh 0.5 --load_model ../exp//tracking/"$1"/model_last.pth
+python test.py tracking --exp_id "$1" --dataset mot --dataset_version 17halfval --track_thresh 0.4 --pre_thresh 0.5 --load_model ../exp//tracking/"$1"/model_last.pth
+# Stop logging
+exit
+
+git add ../data_logging/"$1.txt"
+git commit -m "Run $1 is complete"
+git push 
