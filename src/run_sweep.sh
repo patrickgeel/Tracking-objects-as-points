6506784563b469e8ed3662293f99fa0a3551996e
patrickg <44997541+patrickgeel@users.noreply.github.com>
2022-03-24 12:20:15 +0100

    Update run sweep

--- src/run_sweep.sh
+++ src/run_sweep.sh
@@ -29,7 +29,8 @@ fi
 #Train the model
 if [ "$train" = "$yes" ]
 then 
-python main.py tracking --exp_id "$exp_id" --dataset mot --dataset_version 17halftrain --num_epochs "$num_epochs" --batch_size "$batch_size" --lr_step "$lr_step"
+# python main.py tracking --exp_id "$exp_id" --dataset mot --dataset_version 17halftrain --num_epochs "$num_epochs" --batch_size "$batch_size" --lr_step "$lr_step"
+python main.py tracking --exp_id "$exp_id" --dataset mot --dataset_version 17halftrain --pre_hm --ltrb_amodal --same_aug --hm_disturb 0.05 --lost_disturb 0.4 --fp_disturb 0.1 --num_epochs "$num_epochs" --batch_size "$batch_size" --lr_step "$lr_step"
 fi
 # Setup logging
 echo "Setup logging directory"
@@ -37,7 +38,8 @@ echo "Setup logging directory"
 mkdir -p ../data_logging/
 
 # Run test on the trained model
-python test.py tracking --exp_id "$exp_id" --dataset mot --dataset_version 17halfval --track_thresh 0.4 --pre_thresh 0.5 --load_model ../exp//tracking/"$exp_id"/model_last.pth | tee ../data_logging/"$exp_id.txt"
+# python test.py tracking --exp_id "$exp_id" --dataset mot --dataset_version 17halfval --track_thresh 0.4 --pre_thresh 0.5 --load_model ../exp//tracking/"$exp_id"/model_last.pth | tee ../data_logging/"$exp_id.txt"
+python test.py tracking --exp_id $"exp_id" --dataset mot --dataset_version 17halfval --pre_hm --ltrb_amodal --track_thresh 0.4 --pre_thresh 0.5 --load_model ../exp//tracking/"$exp_id"/model_last.pth| tee ../data_logging/"$exp_id.txt"
 # Stop logging
 
 # git add ../data_logging/"$exp_id.txt"
